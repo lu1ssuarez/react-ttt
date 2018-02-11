e7c9a9063bbf4a4835e7a84e8653cec6693e98a2
Luis Enrique Suárez <luissuarezdev@gmail.com>
2018-02-11 16:32:37 -0400

    X

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,6 +3,30 @@ import React, {Component} from 'react';
 import Square from './Square';
 
 class Board extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            squares: Array(9).fill(null)
+        }
+    }
+
+    handleClick(index) {
+        const squares = this.state.squares.slice();
+
+        squares[index] = 'X';
+
+        this.setState({squares});
+    }
+
+    renderSquare(index) {
+        return (<Square 
+                    value={this.state.squares[index]}
+                    onClick={() => this.handleClick(index)}
+                />);
+    }
+
     render() {
         const status = "Next player: X";
 
@@ -11,19 +35,19 @@ class Board extends Component {
                 <div className="status">{status}</div>
                 <div>
                     <div className="board-row">
-                        <Square />
-                        <Square />
-                        <Square />
+                        {this.renderSquare(0)}
+                        {this.renderSquare(1)}
+                        {this.renderSquare(2)}
                     </div>
                     <div className="board-row">
-                        <Square />
-                        <Square />
-                        <Square />
+                        {this.renderSquare(3)}
+                        {this.renderSquare(4)}
+                        {this.renderSquare(5)}
                     </div>
                     <div className="board-row">
-                        <Square />
-                        <Square />
-                        <Square />
+                        {this.renderSquare(6)}
+                        {this.renderSquare(7)}
+                        {this.renderSquare(8)}
                     </div>
                 </div>
             </div>
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,13 +1,11 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Square extends Component {
-
-    render() {
-        return (
-            <button className="square"></button>
-        );
-    }
-
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
 export default Square;
